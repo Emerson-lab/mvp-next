398c9b1d8354aa1fbd43e7f2216cc10c4328f386
Emerson trindade <trinddade29@gmail.com>
2022-03-13 19:06:14 -0300

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  content: [
+    './src/components/**/*.tsx',
+    './src/pages/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
