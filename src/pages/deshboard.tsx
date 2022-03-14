a231d666b909c62e0093eadbe2497c60f0b20748
Emerson <trinddade29@gmail.com>
2022-03-13 23:43:36 -0300

    create jwt token

---
+++ src/pages/deshboard.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1>Logado com sucesso!</h1>
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,65 @@
+
+import { GetServerSideProps } from "next";
+import { signIn, getSession } from "next-auth/react"
+import { redirect } from "next/dist/server/api-utils";
+
 export default function App() {
+
+  function handlesignIn() {
+    signIn('github');
+  }
+
   return (
-    <nav className="flex sm:justify-center space-x-4">
-      {[
-        ['Home', '/dashboard'],
-        ['Team', '/team'],
-        ['Projects', '/projects'],
-        ['Reports', '/reports'],
-      ].map(([title, url]) => (
-        <a href={url} className=
-          "rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
-      ))}
-    </nav>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <div
+        style={{
+          width: "500px",
+          height: "auto",
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
+        <label>Nome</label>
+        <input type="text" placeholder="Nome" />
+        <label>E-mail</label>
+        <input type="text" placeholder="E-mail" />
+        <button
+          onClick={handlesignIn}
+          style={{
+            border: "1px solid black",
+            marginTop: "10px"
+          }}
+        >
+          Acessar
+        </button>
+      </div>
+    </div>
   )
+}
+
+export const getServersideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/deshboard',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {
+
+    }
+  }
 }
